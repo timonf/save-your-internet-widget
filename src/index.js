40cc027d50c6fc1c3f9bdcc5c30add3dce8f0f72
Timon F <dev@timonf.de>
2019-03-14 16:19:01 +0100

    Added Dutch as new language (thanks to Joshi)

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import html from './templates/widget.html';
 // Available locales
 import * as locales_de from "./lang/de.json";
 import * as locales_en from "./lang/en.json";
+import * as locales_nl from "./lang/nl.json";
 
 const locales = {
   'de': locales_de.default,
   'en': locales_en.default,
+  'nl': locales_nl.default,
 };
 
 // Default configuration
